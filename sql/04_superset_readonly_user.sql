9da31e36ba3bea273fd2780fe1f13cfcc3406dad
Stenzyz <eclipsebeatzzz@icloud.com>
2026-08-14 00:41:25 +0300

    fix CLI superset connection

--- sql/04_superset_readonly_user.sql
+++ sql/04_superset_readonly_user.sql
@@ -1,13 +1,9 @@
-DO
-$$
-BEGIN
-    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = :'ro_user') THEN
-        CREATE USER :ro_user WITH PASSWORD :'ro_password';
-        GRANT USAGE ON SCHEMA marts TO :ro_user;
-        GRANT SELECT ON ALL TABLES IN SCHEMA marts TO :ro_user;
-    END IF;
-END
-$$;
+SELECT format('CREATE USER %I WITH PASSWORD %L', :'ro_user', :'ro_password')
+WHERE NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = :'ro_user')
+\gexec
+
+GRANT USAGE ON SCHEMA marts TO :ro_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA marts TO :ro_user;
 
 ALTER DEFAULT PRIVILEGES FOR ROLE :pg_admin IN SCHEMA marts
     GRANT SELECT ON TABLES TO :ro_user;
